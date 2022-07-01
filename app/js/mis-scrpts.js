56f0e966ee0a4d108e44f9b94bc09a96cf5e810e
Alberth <orlando.alb77@gmail.com>
2022-07-01 11:56:13 -0400

    arreglos metadoll

--- app/js/mis-scrpts.js
+++ app/js/mis-scrpts.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper8", {
   // direction: "vertical",
   //   spaceBetween: 30,
   grabCursor: true,
-  //   loop: true,
+  autoplay: true,
+  loop: true,
   effect: "cards",
 });
